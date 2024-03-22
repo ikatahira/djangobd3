4822e208bc0ef89511358b9e11b701cd37dc59a8
ikatahira <isaque.katahira@fatec.sp.gov.br>
2024-03-22 14:38:21 +0000

    cadastrando

--- pastamodel/projmodel/urls.py
+++ pastamodel/projmodel/urls.py
@@ -4,9 +4,9 @@ from appmodelo import views  # Importe as views do aplicativo appmodelo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.pagina_inicial, name='pagina_inicial'),  # Adicione a view para a página inicial
-    path('clientes/cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),
-    path('clientes/', views.listar_clientes, name='listar_clientes'),
-    path('produtos/cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
-    path('produtos/', views.listar_produtos, name='listar_produtos'),
+    path('', views.pagina_inicial, name='pagina_inicial'),  # Página inicial
+    path('clientes/cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),  # Rota para cadastrar cliente
+    path('clientes/', views.listar_clientes, name='listar_clientes'),  # Rota para listar clientes
+    path('produtos/cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),  # Rota para cadastrar produto
+    path('produtos/', views.listar_produtos, name='listar_produtos'),  # Rota para listar produtos
 ]
